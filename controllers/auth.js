415099818d3787ec9cd8698eb3db712a4a2cd609
David Pérez <dagui7.ipn@gmail.com>
2021-03-08 14:54:33 -0600

    Google Sign in added

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const bcryptjs = require('bcryptjs');
 const Usuario = require('../models/usuario');
 
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 
 
@@ -60,6 +61,57 @@ const login = async(req = request, res = response) => {
 
 }
 
+const googleSignIn = async(req, res = response) => {
+    const { id_token } = req.body;
+
+
+    try {
+        const { correo, nombre, img } = await googleVerify(id_token);
+
+        let usuario = await Usuario.findOne({ correo });
+
+        if (!usuario) {
+            //Tengo que crearlo
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                google: true
+            };
+
+            usuario = new Usuario(data);
+            await usuario.save();
+        }
+
+        if (!usuario.estado) {
+
+            return res.status(401).json({
+                msg: 'El usuario no esta activo, comunicarse con el admin'
+            })
+        }
+
+        //Generar el JWT
+        const token = await generarJWT(usuario.id);
+
+        res.json({
+            usuario,
+            token
+        })
+
+    } catch (error) {
+
+        res.status(400).json({
+            msg: 'Token de google no es válido'
+        })
+
+    }
+
+
+
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 };
--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const { check } = require('express-validator');
 
 const { validarCampos } = require('../middlewares/validar-campos');
 
-const { login } = require('../controllers/auth');
+const { login, googleSignIn } = require('../controllers/auth');
 
 
 const router = Router();
@@ -14,5 +14,9 @@ router.post('/login', [
     validarCampos
 ], login);
 
+router.post('/google', [
+    check('id_token', 'El token es obligatorio').not().isEmpty(),
+    validarCampos
+], googleSignIn);
 
 module.exports = router;
